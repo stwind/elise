37f84b8d4583d03c806213da57e7686b8e625be4
stwind <stwindfy@gmail.com>
2012-11-18 14:45:46 +0800

    One more step towards schema

--- grunt.js
+++ grunt.js
@@ -30,7 +30,7 @@ module.exports = function(grunt) {
     },
     watch: {
       files: '<config:lint.files>',
-      tasks: 'lint test'
+      tasks: 'browserify'
     },
     jshint: {
       options: {
